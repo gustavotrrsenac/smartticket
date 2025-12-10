72c10c3cb2a8967b50b79e535289a5ed3fb5214b
SENAC\vinicius59171446 <vini.oliveira02002@gmail.com>
2025-12-09 21:22:50 -0300

    retorno do codigo app.py semm routes

--- database.py
+++ database.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 db = MySQLDatabase(
-    os.getenv("DB_NAME"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
+    os.getenv("DB_NAME", "smart_ticket"),
+    user=os.getenv("DB_USER","root"),
+    password=os.getenv("DB_PASSWORD","senac"),
     host=os.getenv("DB_HOST", "localhost"),
     port=int(os.getenv("DB_PORT", "3306")),
     charset='utf8mb4'
