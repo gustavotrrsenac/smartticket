1ba11659499df68a5ae8fc4b57a13d79ccb25043
SENAC\yasmin59170596 <yasminribeiropinho@gmail.com>
2025-12-09 21:42:29 -0300

    LINK DAS PAGINAS

--- testesfront.py
+++ testesfront.py
@@ -31,9 +31,21 @@ def cadastro_usuario():
 def cadastro_especialista():
     return render_template('tela_cad_especialista.html')
 
+@app.get("/indicadores")
+def indicadores():
+    return render_template('indicadores.html')
+
+@app.get("/indicadores_link")
+def indicadores_link():
+    return render_template('gerais.html')
+
+@app.get("/meus_chamados")
+def meus_chamados():
+    return render_template('tickets_pessoais.html')
+
 ###################################################
 
 if __name__ == "__main__":
     app.run(debug=True)
     
-    
+    x
